36db7fe954c8144bd228593bd6a6dc29e3170411
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-05-07 15:51:48 +0300

    Add Traceroute Module

---
+++ utils/trace-route.js
@@ -0,0 +1,30 @@
+const Traceroute = require('nodejs-traceroute');
+
+const traceRoute = (destination,callback) => {
+    try{
+        const tracer = new Traceroute();
+        tracer.on('pid', (pid) => {
+            console.log(`pid: ${pid}`);
+        })
+        .on('destination', (destination) => {
+            console.log(`destination: ${destination}`);
+        })
+        .on('hop', (hop) => {
+            if(hop.ip===null){
+               callback(undefined,'Private',undefined);
+            }
+            callback(undefined,undefined,hop);
+        })
+        .on('close', (code) => {
+            console.log(`close: code ${code}`);
+            callback('close',undefined,undefined);
+        }).on('error', (error) => {
+            callback(undefined,error,undefined);
+        });
+        tracer.trace(destination);
+    }catch(ex){
+        console.log(ex);
+    }
+}
+
+module.exports = traceRoute;
